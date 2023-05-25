2efe20a8b7d8bf22a12a97e7701c5e5622d1696d
GabrielTeixeira56 <gabriel_teixeira56@hotmail.com>
2023-05-25 08:13:17 -0300

    restante do curso

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -1,9 +1,11 @@
 //O comando abaixo traz a documentação do cypress ao passar o mouse por cima
 /// <reference types="cypress"/>
 
+import { format } from '../support/utils';
+
 //Pode ser tanto "describe" como "context"
 context('Dev Finances agilizei', () => {
- 
+
   //hooks
   //Trechos de códigos que executam antes e depois do teste
   //before -> executa antes de TODOS os testes
@@ -14,7 +16,7 @@ context('Dev Finances agilizei', () => {
     cy.visit('https://devfinance-agilizei.netlify.app');
     cy.get('#data-table tbody tr').should('have.length', 0);
   });
- 
+
   //Aqui ficam os cenários(it)
   //Cadastrar entradas
   it('Cadastrar entradas', () => {
@@ -24,7 +26,7 @@ context('Dev Finances agilizei', () => {
     - descrever as interações com o cypress
     - adicionar as asserções que precisamos
     */
-    
+
     //para tipo id usar # e tipo class usar .
     cy.get('#transaction .button').click();
     cy.get('#description').type('Mesada');
@@ -53,7 +55,7 @@ context('Dev Finances agilizei', () => {
   //Remover estradas e saídas
   it('Remover entradas e saídas', () => {
     const entrada = 'Mesada';
-    const saida =  'KinderOvo';
+    const saida = 'KinderOvo';
 
     cy.get('#transaction .button').click();
     cy.get('#description').type(entrada);
@@ -78,8 +80,8 @@ context('Dev Finances agilizei', () => {
       .find('img[onclick*=remove]')
       .click();
 
-      cy.get('#data-table tbody tr').should('have.length', 1);
-    
+    cy.get('#data-table tbody tr').should('have.length', 1);
+
     //Estatégia 2: buscar todos os irmãos e buscar o que tem img + attr
     cy.get('td.description')
       .contains(saida)
@@ -87,15 +89,69 @@ context('Dev Finances agilizei', () => {
       .children('img[onclick*=remove]')
       .click();
 
-      cy.get('#data-table tbody tr').should('have.length', 0);
+    cy.get('#data-table tbody tr').should('have.length', 0);
+  })
+
+  it('Validar saldo com diversas transações', () => {
+    const entrada = 'Mesada';
+    const saida = 'KinderOvo';
+
+    cy.get('#transaction .button').click();
+    cy.get('#description').type(entrada);
+    cy.get('[name=amount]').type(100);
+    cy.get('[type=date]').type('2023-05-24');
+    cy.get('button').contains('Salvar').click();
+
+    cy.get('#transaction .button').click();
+    cy.get('#description').type(saida);
+    cy.get('[name=amount]').type(-12);
+    cy.get('[type=date]').type('2023-05-24');
+    cy.get('button').contains('Salvar').click();
+
+    //capturar as linhas com as transações e as colunas com valores
+    //capturar o texto dessas colunas
+    //formatar esses valores das linhas
+    //Somar os valores de entradas e saidas
+    //capturar o texto do total
+    //comparar o somatório de entradas e depesas com o total
+
+    let incomes = 0;
+    let expenses = 0;
+
+    //EACH: serve como um laço de repetição para navegar entre cada linha.
+    //INVOKE: serve para "invocar" uma função em javascript, mesmo passando entre aspas simples o invoke entende que é uma função
+    cy.get('#data-table tbody tr')
+      .each(($el, index, $list) => {
+        cy.get($el).find('td.income, td.expense')
+          .invoke('text').then(texto => {
+
+            if (texto.includes('-')) {
+              expenses = expenses + format(texto);
+            } else {
+              incomes = incomes + format(texto);
+            }
+            cy.log(expenses);
+            cy.log(incomes);
+          });
+
+      });
+
+      cy.get('#totalDisplay').invoke('text').then(texto => {
+        let formattedTotalDisplay = format(texto);
+        let expectedTotal = incomes + expenses;
+
+        expect(formattedTotalDisplay).to.eq(expectedTotal);
+
+      });
+
   })
 
   //trabalhando com outras resoluções
-    //cy.viewport
-    //arquivos de config
-    //passar as configs por linha de comando.
-      //npx cypress open --config viewportWidth= 411,viewportHeight=823
-   
+  //cy.viewport
+  //arquivos de config
+  //passar as configs por linha de comando.
+  //npx cypress open --config viewportWidth= 411,viewportHeight=823
+
   //Execução em modo headless
-    //npx cypress run
+  //npx cypress run
 })
